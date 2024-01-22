1489f7d9b51d5912fa8c8d2dde73a1d0ef7d9a53
somveersingh-23 <somveersingh306599@gmail.com>
2024-01-22 16:41:36 +0530

    upload js

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+function toggleContent() {
+    const hiddenContent = document.querySelector('.hidden-content');
+    hiddenContent.style.display = hiddenContent.style.display === 'none' ? 'block' : 'none';
+}
+</script>
